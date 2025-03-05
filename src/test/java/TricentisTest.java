28fa132c1169d92a93b6c39b2ea93ca1e9396bfb
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-03-05 14:51:10 +0100

    Selenium first draft Tricentis

--- src/test/java/TricentisTest.java
+++ src/test/java/TricentisTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Tricentis {
+public class TricentisTest {
+    @Test
+    public void testTricentis(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.findElement(By.tagName("img")));
+        System.out.println(driver.findElement(By.className("search-box-button")).getDomAttribute("value"));
+        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/ul/li[1]/a")).click();
+        //System.out.println(driver.findElement(By.className("Books")));
+        // driver.quit();
+    }
+
 }
